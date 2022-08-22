2c79be2048f412fd48d79a5c08979951302f8af9
Kuntal Ojha <kuntalojha2021@gmail.com>
2022-08-22 20:24:51 +0530

    DAY 01

---
+++ DAY_01/DAY_01.java
@@ -0,0 +1,10 @@
+// Created by KUNTAL OJHA .
+// Today 22/08/2022 (dd/mm/yyyy)
+// Problem Link: https://rb.gy/q17twr
+// Problem Title: Java Hello World
+
+class Solution{
+    static void printHelloWorld(){
+        System.out.print("Hello World");
+    }
+}
---
+++ DAY_01/DAY_01_HR.java
@@ -0,0 +1,15 @@
+// Created by KUNTAL OJHA .
+// Today 22/08/2022 (dd/mm/yyyy)
+// Problem Link: https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+// Problem Title: Welcom to Java.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
