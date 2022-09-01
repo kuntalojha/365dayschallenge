d261baf5c0c880ac0f8c0f1874e9dde986c2d84b
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-09-02 00:02:51 +0530

    Update DAY_02.java

--- DAY_11/DAY_02.java
+++ DAY_11/DAY_02.java
@@ -1,7 +1,7 @@
 // Created by KUNTAL OJHA .
-// Today 23/08/2022 (dd/mm/yyyy)
-// Problem Link: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
-// Problem Title:  Java Stdin and Stdout I.
+// Today 1/09/2022 (dd/mm/yyyy)
+// Problem Link: https://www.hackerrank.com/challenges/java-int-to-string/problem?isFullScreen=true
+// Problem Title:  Java Int to String.
 
 import java.io.*;
 import java.util.*;
@@ -9,15 +9,9 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner kuntal = new Scanner (System.in);
-        int m = kuntal.nextInt();
-        int n = kuntal.nextInt();
-        int o = kuntal.nextInt();
-
-        System.out.println(m);
-        System.out.println(n);
-        System.out.println(o);
-
+        Scanner kuntal = new Scanner(System.in);
+        int x = kuntal.nextInt();
+        String s =String.valueOf(x);
+        System.out.println("Good job");
     }
 }
