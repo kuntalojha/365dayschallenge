e8913f926c4471f89c7ca737ff0ce423643f2cb9
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-09-13 22:02:41 +0530

    Update and rename DAY_02.java to DAY_12.java

---
+++ DAY_12/DAY_12.java
@@ -0,0 +1,30 @@
+// Created by KUNTAL OJHA .
+// Today 13/09/2022 (dd/mm/yyyy)
+// Problem Link: https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=false
+// Problem Title: Java Currency Formatter.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        String us=NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india=NumberFormat.getCurrencyInstance(new Locale ("en","in")).format(payment);
+        String china=NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france=NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
