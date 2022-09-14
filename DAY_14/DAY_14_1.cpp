7bc8327284141ee1c78187b62d506dd5086b90e2
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-09-14 23:26:04 +0530

    Update and rename DAY_01.cpp to DAY_14_1.cpp

---
+++ DAY_14/DAY_14_1.cpp
@@ -0,0 +1,27 @@
+// Created by KUNTAL OJHA .
+// Today 14/09/2022 (dd/mm/yyyy)
+// Problem Link: https://www.codechef.com/submit/MANIPULATE
+// Problem Title: Ezio and Guards.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y)
+	    {
+	        cout<<"YES"<<endl;
+	    }else 
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
