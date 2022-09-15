b5bea3c141e2daa07b39134e643dceb03cdb9bad
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-09-15 23:26:21 +0530

    Update DAY_02.java

--- DAY_14/DAY_02.java
+++ DAY_14/DAY_02.java
@@ -1,7 +1,7 @@
 // Created by KUNTAL OJHA .
-// Today 23/08/2022 (dd/mm/yyyy)
-// Problem Link: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
-// Problem Title:  Java Stdin and Stdout I.
+// Today 14/09/2022 (dd/mm/yyyy)
+// Problem Link: https://www.hackerrank.com/challenges/java-strings-introduction/problem
+// Problem Title:  Java String Introduction.
 
 import java.io.*;
 import java.util.*;
@@ -10,14 +10,18 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner kuntal = new Scanner (System.in);
-        int m = kuntal.nextInt();
-        int n = kuntal.nextInt();
-        int o = kuntal.nextInt();
+        Scanner kuntal = new Scanner(System.in);
+        String a = kuntal.nextLine();
+        String b = kuntal.nextLine();
+        System.out.println(a.length()+b.length());
+        
+    if ((int)a.charAt(0)>(int)b.charAt(0)) 
+           System.out.println("Yes") ;
+        else 
+            System.out.println("No");
+        a = a.substring(0, 1).toUpperCase()+a.substring(1,a.length());
+        b = b.substring(0, 1).toUpperCase()+b.substring(1,b.length());
 
-        System.out.println(m);
-        System.out.println(n);
-        System.out.println(o);
-
+        System.out.println(a + " "+ b);
     }
 }
