059faf6392ddc418ed6bcbcc98488df151947e24
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-09-16 22:48:35 +0530

    Update DAY_02.java

--- DAY_15/DAY_02.java
+++ DAY_15/DAY_02.java
@@ -1,23 +1,22 @@
 // Created by KUNTAL OJHA .
-// Today 23/08/2022 (dd/mm/yyyy)
-// Problem Link: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
-// Problem Title:  Java Stdin and Stdout I.
+// Today 15/09/2022 (dd/mm/yyyy)
+// Problem Link: https://www.hackerrank.com/challenges/java-substring/problem?isFullScreen=true
+// Problem Title: Java Substring.
 
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner kuntal = new Scanner (System.in);
-        int m = kuntal.nextInt();
-        int n = kuntal.nextInt();
-        int o = kuntal.nextInt();
-
-        System.out.println(m);
-        System.out.println(n);
-        System.out.println(o);
-
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        System.out.println(S.substring(start,end));
     }
 }
