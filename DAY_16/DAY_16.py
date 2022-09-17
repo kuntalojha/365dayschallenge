a70a1f2dfa4c5d78babcddb4e1002d4ccaf368fe
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-09-17 23:49:11 +0530

    Update and rename DAY_01.py to DAY_16.py

---
+++ DAY_16/DAY_16.py
@@ -0,0 +1,4 @@
+// Created by KUNTAL OJHA .
+// Today 16/09/2022 (dd/mm/yyyy)
+// Problem Link: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=false
+// Problem Title: Find the Runner-Up Score!.
