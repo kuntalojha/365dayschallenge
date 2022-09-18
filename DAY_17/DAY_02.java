57ea85d1b3bad49912ec7aa45481002fbda5df7b
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-09-18 23:22:28 +0530

    Update DAY_02.java

--- DAY_17/DAY_02.java
+++ DAY_17/DAY_02.java
@@ -1,23 +1,22 @@
 // Created by KUNTAL OJHA .
-// Today 23/08/2022 (dd/mm/yyyy)
-// Problem Link: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
-// Problem Title:  Java Stdin and Stdout I.
+// Today 17/09/2022 (dd/mm/yyyy)
+// Problem Link: https://www.hackerrank.com/challenges/java-biginteger/problem
+// Problem Title: Java BigInteger.
 
 import java.io.*;
 import java.util.*;
+import java.math.BigInteger;  
 
 public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner kuntal = new Scanner (System.in);
-        int m = kuntal.nextInt();
-        int n = kuntal.nextInt();
-        int o = kuntal.nextInt();
-
-        System.out.println(m);
-        System.out.println(n);
-        System.out.println(o);
-
+        
+        Scanner kuntal = new Scanner(System.in);
+        BigInteger A = kuntal.nextBigInteger();
+        BigInteger B = kuntal.nextBigInteger();
+        
+        System.out.println(A.add(B));
+        System.out.println(A.multiply(B));
     }
 }
