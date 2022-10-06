d617840a1197549c27ff5fdc6135d21b98bf8f85
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-10-06 23:59:54 +0530

    Update and rename DAY_01.cpp to DAY_35.cpp

---
+++ DAY_35/DAY_35.cpp
@@ -0,0 +1,29 @@
+// Created by KUNTAL OJHA .
+// Today 06/10/2022 (dd/mm/yyyy)
+// Problem Link: https://www.codechef.com/submit/AUDIBLE
+// Problem Title: Audible Range
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(67<=x && x<=45000)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else 
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
+
