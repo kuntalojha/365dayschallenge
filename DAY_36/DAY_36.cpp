edf8744da657127b2922c08112b68ee86094a28f
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-10-07 00:16:06 +0530

    Update and rename DAY_01.cpp to DAY_36.cpp

---
+++ DAY_36/DAY_36.cpp
@@ -0,0 +1,28 @@
+// Created by KUNTAL OJHA .
+// Today 07/10/2022 (dd/mm/yyyy)
+// Problem Link: https://www.codechef.com/submit/REACHTARGET
+// Problem Title: Reach the Target
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(67<=x && x<=45000)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else 
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
