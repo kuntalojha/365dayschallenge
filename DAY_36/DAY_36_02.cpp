b3e2ac1ea8bb1c289bb4669d5f457a1f38885e74
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-10-07 22:22:11 +0530

    Create DAY_36_02.cpp

---
+++ DAY_36/DAY_36_02.cpp
@@ -0,0 +1,21 @@
+// Created by KUNTAL OJHA .
+// Today 07/10/2022 (dd/mm/yyyy)
+// Problem Link: https://www.codechef.com/OCT221D/problems/PODIUM
+// Problem Title: Podium Finish
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<a+b<<endl;
+	}
+	return 0;
+}
