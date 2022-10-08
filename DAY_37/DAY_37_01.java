2ee4e47cf4afc830bac7ba186b50b5434b01775f
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-10-08 18:36:53 +0530

    Update and rename DAY_02.java to DAY_37_01.java

---
+++ DAY_37/DAY_37_01.java
@@ -0,0 +1,51 @@
+
+// Created by KUNTAL OJHA .
+// Today 08/10/2022 (dd/mm/yyyy)
+// Problem Link: https://www.codechef.com/OCT221D/problems/BUILDINGRACE
+// Problem Title: Building Race
+
+
+
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner kuntal = new Scanner(System.in);
+		int t = kuntal.nextInt();
+		for(int i=0;i<t;i++)
+		{
+            float a,b,x,y,n,m;
+            a = kuntal.nextFloat();
+            b = kuntal.nextFloat();
+            x = kuntal.nextFloat();
+            y = kuntal.nextFloat();
+
+            n=a/x;
+            m=b/y;
+            
+            if(n==m)
+            {
+                System.out.println("Both");
+            }else if (n<m)
+            {
+                System.out.println("Chef");
+            }else
+            {
+                System.out.println("Chefina");
+            }
+
+            
+		}
+
+	}
+}
+
