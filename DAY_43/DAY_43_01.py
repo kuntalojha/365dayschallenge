cffef27ca0f166e9366e08370801439daafe82f9
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-10-14 09:08:35 +0530

    Update and rename DAY_41_01.py to DAY_43_01.py

---
+++ DAY_43/DAY_43_01.py
@@ -0,0 +1,20 @@
+# Created by KUNTAL OJHA .
+# Today 12/10/2022 (dd/mm/yyyy)
+# Problem Link: https://www.codechef.com/submit/DISCNT
+# Problem Title: Discounte:
+# Programmng Language: PYTHON
+
+  
+  
+def main():
+    t = int(input())
+    i=0 
+    while i<t:
+      i=i+1
+      x=int(input())
+      print (100-x)
+
+if __name__=='__main__':
+    main()
+
+    
