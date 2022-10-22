d641e6944800ece5c814300ef28fda7e69e40b37
Kuntal Ojha <67447627+kuntalojha@users.noreply.github.com>
2022-10-22 23:58:06 +0530

    Update DAY_41_01.java

--- DAY_51/DAY_41_01.java
+++ DAY_51/DAY_41_01.java
@@ -1,26 +1,41 @@
 // Created by KUNTAL OJHA .
-// Today 12/10/2022 (dd/mm/yyyy)
-// Problem Link: 
-// Problem Title: 
+// Today 22/10/2022 (dd/mm/yyyy)
+// Problem Link: https://www.codechef.com/problems/BIRDFARM?tab=statement
+// Problem Title: Chef and Bird farm
 // Programmng Language: JAVA
 
 
 
-import java.io.*;
+/* package codechef; // don't place package name! */
+
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Solution {
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner kuntal = new Scanner(System.in);
+		int t = kuntal.nextInt();
+		for(int i=0;i<t;i++)
+		{
+	    	int x = kuntal.nextInt();
+	    	int y = kuntal.nextInt();
+	    	int z = kuntal.nextInt();
+		    
+		    if (z%x==0 && z%y==0) 
+		        System.out.println("ANY");
+		    else if (z%x==0) 
+		        System.out.println("CHICKEN");
+		    else if(z%y==0) 
+		        System.out.println("DUCK");
+		    else 
+		        System.out.println("NONE");
+            
+		}
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner kuntal = new Scanner (System.in);
-        int m = kuntal.nextInt();
-        int n = kuntal.nextInt();
-        int o = kuntal.nextInt();
-
-        System.out.println(m);
-        System.out.println(n);
-        System.out.println(o);
-
-    }
+	}
 }
